d9c1f8f44b0048fdab54d612549cd802153d71a7
GTourinho <gabrieltourinho@hotmail.com>
2022-08-20 15:45:51 -0300

    Load hair_2 via script

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.U2D.Animation;
 
 public class PlayerController : MonoBehaviour
 {
@@ -9,17 +10,32 @@ public class PlayerController : MonoBehaviour
     public float moveSpeed = 1f;
     public float collisionOffset = 0.05f;
     public ContactFilter2D movementFilter;
+    public SpriteLibrary spriteLibrary;
     List<RaycastHit2D> castCollisions = new List<RaycastHit2D>();
-
     Vector2 movementInput;
+    Animator animator;
     Rigidbody2D rb;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        spriteLibrary = this.transform.Find("hair").GetComponent<SpriteLibrary>();
+        animator.enabled = false;
+        
+        UnityEditor.AssetDatabase.Refresh();
+
+        SpriteLibraryAsset spriteLibraryAsset = Resources.Load<SpriteLibraryAsset>("Hair_2");
+
+        spriteLibrary.spriteLibraryAsset = spriteLibraryAsset;
+
+        animator.enabled = true;
+        
     }
 
     private void FixedUpdate() {
+
+
         if(movementInput != Vector2.zero){  
             bool success = TryMove(movementInput);
             if(!success){
@@ -28,6 +44,15 @@ public class PlayerController : MonoBehaviour
                     success = TryMove(new Vector2(0, movementInput.y));
                 }
             }
+            animator.SetBool("isMoving", success);
+            // get all sprite library assets
+            
+
+
+
+        } else{
+            
+            animator.SetBool("isMoving", false);
         }
     }
 
